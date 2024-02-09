319fe601f2e6430c615c8ecde426634bf8c5899c
kalpanakarunarathna@gmail.com <kalpanakarunarathna@gmail.com>
2024-02-09 22:35:37 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,15 +1,29 @@
 <footer>
-        <br>
-        <p>Follow Little Tarts</p>
-        <img class="mail" src="Pictures/email_.png" alt="mail logo">
-        <img class="fb" src="Pictures/facebook-box_.png" alt="fb logo">
-        <img class="insta" src="Pictures/instagram_.png" alt="insta logo"><br>
-        <p>&copy; Little Tart Bakery. All Rights Reserved. 2022</p>
-        <br>
-    </footer>
+            <br>
+            <p>Follow Little Tarts</p>
+            <img class="mail" src="Pictures/email_.png">
+            <img class="fb" src="Pictures/facebook-box_.png">
+            <img class="insta" src="Pictures/instagram_.png"><br>
+            <p>&copy; Little Tart Bakery. All Rights Reserved. 2022</p>
+            <br>
 
-</div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> 
+            <nav class="navbar bg-light">
+              <div class="container-fluid">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link" href="career.php">Career</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="review.php">Customer Review</a>
+                  </li>
+                </ul>
+            </div>
+            </nav>
+ 
 
+
+        </footer>
+    </div>  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> 
 </body>
 </html>
