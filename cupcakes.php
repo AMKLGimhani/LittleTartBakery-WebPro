f4e6d0d361df54a6dfc529a858320d21eff1dd18
kalpanakarunarathna@gmail.com <kalpanakarunarathna@gmail.com>
2024-02-09 11:50:24 +0200

    Update cupcakes.php

---
+++ cupcakes.php
@@ -0,0 +1,94 @@
+<?php
+include 'header.php';
+?>
+
+<div class="row">          
+            <a href="cake.html"><img class="Discount" src="Pictures/Pink and Red Happy Valentine’s Day Sale Promotional Banner.jpg" alt="Valentine cake"></a>    
+        </div>
+
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="Pictures/cupcake1Bday_Assortment.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Birthday Cupcakes</h5>
+                        <p class="card-text">30,00 Euro</p>
+                        <a href="shop.php" class="btn btn-danger">Shop Now</a>
+                    </div>
+                </div>
+            </div> 
+
+            <br>
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="Pictures/cupcake2Valentine.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Valantine Cupcakes</h5>
+                        <p class="card-text">56,00 Euro</p>
+                        <a href="shop.php" class="btn btn-danger">Shop Now</a>
+                    </div>
+                </div>
+            </div>
+
+            <br>           
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="Pictures/cupcake3Classic_Assortment.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Classic Cupcakes</h5>
+                        <p class="card-text">30,00 Euro</p>
+                        <a href="shop.php" class="btn btn-danger">Shop Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="Pictures/cupcake4Confetti.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Confetti Cupcakes</h5>
+                        <p class="card-text">56,00 Euro</p>
+                        <a href="shop.php" class="btn btn-danger">Shop Now</a>
+                    </div>
+                </div>
+            </div> 
+
+            <br>
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="Pictures/cupcake5Mini_Van.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Mini Vanilla Cupcakes</h5>
+                        <p class="card-text">35,00 Euro</p>
+                        <a href="shop.php" class="btn btn-danger">Shop Now</a>
+                    </div>
+                </div>
+            </div>
+
+            <br>           
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="Pictures/cupcake6Rose.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Rose Cupcakes</h5>
+                        <p class="card-text">50,00 Euro</p>
+                        <a href="shop.php" class="btn btn-danger">Shop Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+<?php
+include 'footer.php';
+?>
