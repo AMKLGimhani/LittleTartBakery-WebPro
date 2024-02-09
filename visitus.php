72df452da9443f4056bced99f39051937edc8035
kalpanakarunarathna@gmail.com <kalpanakarunarathna@gmail.com>
2024-02-09 11:46:12 +0200

    update

---
+++ visitus.php
@@ -0,0 +1,64 @@
+<?php
+include 'header.php';
+?>
+
+
+<div class="row">
+            <img class="visitus" src="Pictures/visitus.jpg" alt="visit us banner">
+        </div>
+
+        <div class="row">
+            <div class="col-md-6">
+                <div class="branch-card">
+                    <h2>Main Branch</h2><p>Little Tart,<br>Rauhankatu,<br> 5, 13100,<br> Hämeenlinna<br>Tel:+358 0402542598 </p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="branch-card">
+                    <h2>Helsinki</h2><p>Little Tart,<br>Lammaskuja,<br> 11B,<br> Helsinki<br>Tel:+358 0403582532 </p>
+                </div>
+            </div>
+
+        </div>
+        
+        <div class="row">
+            <div class="col-md-6">
+                <div class="branch-card">
+                    <h2>Tampare</h2><p>Little Tart,<br>Hatanpään valtatie,<br> 38, 33900,<br>Tampare<br>Tel:+358 0407852222 </p>
+                </div>
+            </div>
+            <div class="col-md-6">   
+                <div class="branch-card">
+                    <h2>Turku</h2><p>Little Tart,<br>Eerikinkatu,<br> 16, 20100,<br> Turku<br>Tel:+358 0403572541 </p>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="col-md-4">
+                <div class="social1">
+                    <img class="vsmail" src="Pictures/email_.png" alt="mail logo"><br><p>Email Address <br>littletart@gmail.com</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="social2">
+                    <img class="vsfb" src="Pictures/facebook-box_.png" alt="fb logo"><br><p>Facebook <br>Little Tart</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="social3">
+                    <img class="vsinsta" src="Pictures/instagram_.png" alt="instagram logo"><br><p>Instagram <br>Little Tart</p>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+<?php
+include 'footer.php';
+?>
