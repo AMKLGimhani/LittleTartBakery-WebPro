41e93b8fcecbdadba7af066819bdd92b589ef027
kalpanakarunarathna@gmail.com <kalpanakarunarathna@gmail.com>
2024-02-13 22:42:52 +0200

    update

--- shop.php
+++ shop.php
@@ -1,43 +1,5 @@
-<<<<<<< HEAD
-<?php   
-<<<<<<<< HEAD:shop.php
-$title = "Shopping Cart";
- session_start(); 
- include 'header.php'; 
- include 'dbmember.php'; 
-//manasha
- if(isset($_POST["add_to_cart"]))  
- {  
-      if(isset($_SESSION["shopping_cart"]))  
-      {  
-           $item_array_id = array_column($_SESSION["shopping_cart"], "item_array");  
-          
-           
-           if(!in_array($_GET["id"], $item_array_id))  
-           {  
-                $count = count($_SESSION["shopping_cart"]);  
-                
-                $item_array = array(  
-                     'item_id'=>$_GET["id"],  
-                     'item_name'=>$_POST["hidden_name"],  
-                     'item_price'=>$_POST["hidden_price"],  
-                     'item_quantity'=>$_POST["quantity"]  
-                );  
-                $_SESSION["shopping_cart"][$count] = $item_array;  
-           }  
-           else  
-           {  
-     
-                echo '<script>window.location="process.php"</script>';  
-           }  
-      }  
-      else  
-      {  
-========
-=======
 <?php  
 $title = "Shop here";
->>>>>>> 24d5b111afac590c21e86d2be0449c0b4a2076b4
 session_start(); 
 include 'header.php'; 
 include 'dbshop.php'; // calling the database connection.
@@ -45,10 +7,6 @@ include 'dbshop.php'; // calling the database connection.
 
 <?php
      if(isset($_POST["add_to_cart"])) {  // checking if the 'add_to_cart' form has been submitted.
-<<<<<<< HEAD
->>>>>>>> 24d5b111afac590c21e86d2be0449c0b4a2076b4:process.php
-=======
->>>>>>> 24d5b111afac590c21e86d2be0449c0b4a2076b4
          
           if(isset($_SESSION["shopping_cart"])) {  //checking for 'shopping cart session variable.
                $item_array_id = array_column($_SESSION["shopping_cart"], "item_array"); //creates an array containing all the "item_array" values in the shopping cart.      
@@ -64,11 +22,7 @@ include 'dbshop.php'; // calling the database connection.
                     );  
                     //adds the new item array to the $_SESSION["shopping_cart"] array at the index of $count.
                     $_SESSION["shopping_cart"][$count] = $item_array; 
-<<<<<<< HEAD
-                    echo '<script>window.location="process.php"</script>';  //redirects the user to "process.php.
-=======
                     echo '<script>window.location="shop.php"</script>';  //redirects the user to "process.php.
->>>>>>> 24d5b111afac590c21e86d2be0449c0b4a2076b4
                }  
              
           }  
@@ -93,11 +47,7 @@ include 'dbshop.php'; // calling the database connection.
                     if($values["item_id"] == $_GET["pid"]) {  //It checks $values["item_id"] is equal to the ID passed through ($_GET["id"]). 
                          $key = array_search($_GET['pid'], $_SESSION['shopping_cart']);	
                          unset($_SESSION["shopping_cart"][$key]);   //removes the item from the $_SESSION["shopping_cart"].                   
-<<<<<<< HEAD
-                         echo '<script>window.location="process.php"</script>';  //redirect
-=======
                          echo '<script>window.location="shop.php"</script>';  //redirect
->>>>>>> 24d5b111afac590c21e86d2be0449c0b4a2076b4
                     }  
                } 
                $_SESSION['shopping_cart'] = array_values($_SESSION['shopping_cart']); 
@@ -125,11 +75,7 @@ include 'dbshop.php'; // calling the database connection.
                                    when the form is submitted(clicked on add to cart button) selected product 
                                     will be added to the shopping cart.<-->
                                    <div class="col-sm-6"> 
-<<<<<<< HEAD
-                                        <form method="post" action="process.php?action=add&id="<?php echo $row["pid"]; ?> >  
-=======
                                         <form method="post" action="shop.php?action=add&id="<?php echo $row["pid"]; ?> >  
->>>>>>> 24d5b111afac590c21e86d2be0449c0b4a2076b4
                                              <div style="border:1px solid #333; background-color:#f1f1f1; border-radius:5px; padding:16px;" align="center">  
                                                   <img src="<?php echo $row["image"]; ?>" width="150" height="auto" /><br />  
                                                   <h4 class="text-basic" ><?php echo $row["name"]; ?></h4>  
@@ -143,46 +89,6 @@ include 'dbshop.php'; // calling the database connection.
                                         </form>  
                                    </div>  
 
-<<<<<<< HEAD
-<<<<<<<< HEAD:shop.php
-                              }
-                            
-                              
-                    ?>   
-                        <div class="col-sm-6">
-
-                         <form method="post" action="shop_process.php"<?php echo $row["id"]; ?> >  
-                              <div style="border:1px solid #333; background-color:#f1f1f1; border-radius:5px; padding:16px;" align="center">  
-                                   <img src="<?php echo $row["image"]; ?>" width="150" height="auto" /><br />  
-                                   <h4 class="text-basic" ><?php echo $row["name"]; ?></h4>  
-                                   <h4 class="text-primary">€ <?php echo $row["price"]; ?></h4>  
-                                   <input type="number" name="quantity" class="form-control" value="1" />  
-                                   <input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>" />  
-                                   <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" />  
-                                   <input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-success" value="Add to Cart" />  
-                              </div>  
-                         </form>  
-                    </div>  
-
-                    <?php  
-                         $col++;
-                         if ($col==3)
-                         {
-                             $col=1;
-                         }
-                 
-                         if ($col==1)
-                         {
-                         echo "</div>"; 
-                         } 
-                         
-                        
-                    }   
-                    }  
-                    ?>
-========
-=======
->>>>>>> 24d5b111afac590c21e86d2be0449c0b4a2076b4
                                    <?php  
                                         $col++;
                                         if ($col==3) {
@@ -194,80 +100,10 @@ include 'dbshop.php'; // calling the database connection.
                               }   
                          }  
                                    ?>
-<<<<<<< HEAD
->>>>>>>> 24d5b111afac590c21e86d2be0449c0b4a2076b4:process.php
-=======
->>>>>>> 24d5b111afac590c21e86d2be0449c0b4a2076b4
 
                </div>
                
                <div class="col-6">
-<<<<<<< HEAD
-<<<<<<<< HEAD:shop.php
-
-               <h3 align="center">Order Details</h3>  
-                <div class="table-responsive">  
-                <table class="table table-bordered">  
-    <tr>  
-        <th width="40%">Item Name</th>  
-        <th width="10%">Quantity</th>  
-        <th width="20%">Price</th>  
-        <th width="15%">Total</th>  
-        <th width="5%">Action</th>  
-    </tr>  
-    <?php   
-    if(!empty($_SESSION["shopping_cart"]))  
-    {  
-        $total = 0;
-        $totalCount = 0; // Initialize total count variable
-        foreach($_SESSION["shopping_cart"] as $keys => $values)  
-        {  
-            ?>  
-            <tr>  
-                <td><?php echo $values["item_name"]; ?></td>  
-                <td><?php echo $values["item_quantity"]; ?></td>  
-                <td>€ <?php echo $values["item_price"]; ?></td>  
-                <td>€ <?php echo number_format($values["item_quantity"] * $values["item_price"], 2); ?></td>  
-                <td><a href="process.php?action=delete&id=<?php echo $values["item_id"]; ?>"><span class="text-danger">Remove</span></a></td>  
-            </tr>  
-            <?php  
-            $total += ($values["item_quantity"] * $values["item_price"]);
-            $totalCount += $values["item_quantity"]; // Increment total count by item quantity
-        }  
-        ?>  
-        <tr>  
-            <td colspan="3" align="right">Total</td>  
-            <td align="right">€ <?php echo number_format($total, 2); ?></td>  
-            <td></td>  
-        </tr>  
-        <tr>
-            <td colspan="4" align="right">Total Count</td>
-            <td><?php echo $totalCount; ?></td>
-        </tr>
-
-        
-        <?php  
-    }  
-    ?>  
-</table> 
-
-          </div>
-
-                <button type="submit" class="btn btn-primary" name="Submitorder">Submit</button>
-        </div>
-                     </div>
-             
-
-               </div>
-          </div>  
-     </div>  
-
-     
-<br />  
-<?php require_once 'footer.php'; 
-========
-=======
->>>>>>> 24d5b111afac590c21e86d2be0449c0b4a2076b4
                     <h4 align="center">Your Order Details</h4>  <br>
                     <div class="table-responsive">  
                          <table class="table table-bordered">  
@@ -290,11 +126,7 @@ include 'dbshop.php'; // calling the database connection.
                                    <td><?php echo $values["item_quantity"]; ?></td>  
                                    <td>€ <?php echo $values["item_price"]; ?></td>  
                                    <td>€ <?php echo number_format($values["item_quantity"] * $values["item_price"], 2); ?></td>  
-<<<<<<< HEAD
-                                   <td><a href="process.php?action=delete&id=<?php echo $values["item_id"]; ?>"><span class="text-danger">Remove</span></a></td> 
-=======
                                    <td><a href="shop.php?action=delete&id=<?php echo $values["item_id"]; ?>"><span class="text-danger">Remove</span></a></td> 
->>>>>>> 24d5b111afac590c21e86d2be0449c0b4a2076b4
                               </tr>  
                                    <?php  
                                         $total += ($values["item_quantity"] * $values["item_price"]);// calculating total price.
@@ -324,10 +156,6 @@ include 'dbshop.php'; // calling the database connection.
 <?php 
 mysqli_close($conn);
 include 'footer.php';
-<<<<<<< HEAD
->>>>>>>> 24d5b111afac590c21e86d2be0449c0b4a2076b4:process.php
-=======
->>>>>>> 24d5b111afac590c21e86d2be0449c0b4a2076b4
 ?>
 
 
