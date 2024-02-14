99742a71eaed13c0276abefc58a79707602c5d94
Lakshika Gimhani <127098842+AMKLGimhani@users.noreply.github.com>
2024-02-14 11:30:09 +0200

    Admin view page added

---
+++ admin_view.php
@@ -0,0 +1,18 @@
+<?php
+$title = "Admin view";
+require_once 'layout/header.php';
+?>
+
+<div class="row">
+    <div class="col-md-6">
+        <a href="career_read.php" class="btn btn-danger">View Receied Appications</a>
+    </div>
+    <div class="col-md-6">
+    
+    </div>
+
+</div>
+
+<?php
+require_once 'layout/footer.php';
+?>
