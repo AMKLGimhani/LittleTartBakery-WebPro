4acf96578c966e08ac60065b13987658e3be2ecd
Lakshika Gimhani <127098842+AMKLGimhani@users.noreply.github.com>
2024-02-14 11:46:20 +0200

    update admin page

--- admin_view.php
+++ admin_view.php
@@ -3,13 +3,14 @@ $title = "Admin view";
 require_once 'layout/header.php';
 ?>
 
+<h2>Admin Page</h2><br>
 <div class="row">
     <div class="col-md-6">
-        <a href="career_read.php" class="btn btn-danger">View Received Career Appications</a><br><br>
+        <br><br><a href="career_read.php" class="btn btn-danger">View Received Career Appications</a><br><br>
         <a href="member_read.php" class="btn btn-danger">View Membership Details</a>
     </div>
     <div class="col-md-6">
-    
+        <img class="img-fluid" src="Pictures\admin.png" alt="adminpic" style="height: 500px; width:500px">
     </div>
 
 </div>
