3016a9035f1ad437e876861661cc2e8b2a8dfe1c
Lakshika Gimhani <127098842+AMKLGimhani@users.noreply.github.com>
2024-02-14 11:35:12 +0200

    update admin page

--- admin_view.php
+++ admin_view.php
@@ -5,7 +5,8 @@ require_once 'layout/header.php';
 
 <div class="row">
     <div class="col-md-6">
-        <a href="career_read.php" class="btn btn-danger">View Receied Appications</a>
+        <a href="career_read.php" class="btn btn-danger">View Received Career Appications</a><br><br>
+        <a href="member_read.php" class="btn btn-danger">View Membership Details</a>
     </div>
     <div class="col-md-6">
     
